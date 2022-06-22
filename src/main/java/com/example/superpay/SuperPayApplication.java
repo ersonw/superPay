4721bfbd53adb3c4a57588c78dc31d51b8c8ce40
ersonw <asoonnode@gmail.com>
2022-06-22 16:54:05 +0800

    first commit

--- src/main/java/com/example/superpay/SuperPayApplication.java
+++ src/main/java/com/example/superpay/SuperPayApplication.java
@@ -2,12 +2,18 @@ package com.example.superpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SuperPayApplication {
+public class SuperPayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperPayApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SuperPayApplication.class);
 	}
 
 }
