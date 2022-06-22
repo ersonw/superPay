b03973d0a951403061e007704c23788d6e0b96c5
ersonw <asooonnode@gmail.com>
2022-06-23 04:47:41 +0800

    202206230448

--- src/main/java/com/example/superpay/control/UserControl.java
+++ src/main/java/com/example/superpay/control/UserControl.java
@@ -50,21 +50,21 @@ public class UserControl {
 //                                      @RequestParam(value = "ip") @ApiParam(hidden = true) String ip
     ){
 
-        for (int i = 0; i < 10; i++) {  //增加一条记录
-            User user = new User();
-            user.setAddTime(System.currentTimeMillis());
-            user.setFee(1);
-            user.setCallbackUrl("sdasd");
-            user.setNotifyUrl("sdasd");
-            user.setPassword("dsads");
-            user.setUsername("dsads");
-            user.setSalt("dsads");
-            user.setRate(1);
-            user.setState(1);
-            user.setSecretKey("dasd");
-//            user.setId(mongoAutoidUtil.getNextSequence("user"));
-            mongoTemplate.save(user);
-        }
+//        for (int i = 0; i < 10; i++) {  //增加一条记录
+//            User user = new User();
+//            user.setAddTime(System.currentTimeMillis());
+//            user.setFee(1);
+//            user.setCallbackUrl("sdasd");
+//            user.setNotifyUrl("sdasd");
+//            user.setPassword("dsads");
+//            user.setUsername("dsads");
+//            user.setSalt("dsads");
+//            user.setRate(1);
+//            user.setState(1);
+//            user.setSecretKey("dasd");
+////            user.setId(mongoAutoidUtil.getNextSequence("user"));
+//            mongoTemplate.save(user);
+//        }
 
 //        Iterable<User> articles = userRepository.findAll();
 //        articles.forEach(article2 -> {
