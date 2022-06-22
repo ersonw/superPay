8de0de374f9bf985848a68e278263c3c456389d1
ersonw <asoonnode@gmail.com>
2022-06-22 16:53:26 +0800

    first commit

---
+++ src/test/java/com/example/superpay/SuperPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
