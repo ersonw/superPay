b0215a083f80065733a075f75e2750eb0bbf46ad
ersonw <asooonnode@gmail.com>
2022-06-26 13:57:50 +0800

    202206261358

--- src/main/java/com/example/superpay/config/AssertConfig.java
+++ src/main/java/com/example/superpay/config/AssertConfig.java
@@ -15,9 +15,9 @@ public class AssertConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").addResourceLocations( "classpath:/META-INF/resources/static/");
-        registry.addResourceHandler("/admin/**").addResourceLocations( "static/");
-        registry.addResourceHandler("/upload/**").addResourceLocations( "static/upload/");
-        registry.addResourceHandler("index.html").addResourceLocations( "/");
+        registry.addResourceHandler("/admin/**").addResourceLocations( "classpath:/static/");
+        registry.addResourceHandler("/upload/**").addResourceLocations( "classpath:/static/upload/");
+        registry.addResourceHandler("index.html").addResourceLocations( "classpath:/");
         //从这里开始，是我加的swagger的静态资源
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
